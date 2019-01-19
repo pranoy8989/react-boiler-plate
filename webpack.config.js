1b3d26e73cae2bff7671eacef0f2045e0e5adc1b
Pranoy Dutta <pranoy.iem@gmail.com>
2019-01-19 17:04:58 +0530

    added react, enzyme, source map

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+let nodeExternals = require('webpack-node-externals');
+
+const moduleObj = {
+  loaders: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ["babel-loader"],
+    }
+  ],
+};
+
+const client = {
+  // devtool: 'cheap-module-source-map',
+  entry: {
+    'client': './src/client/index.js'
+  },
+  target: 'web',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/public')
+  },
+  module: moduleObj,
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'src/client/index.html'
+    })
+  ]
+};
+const server = {
+  entry: {
+    'server': './src/server/index.js'
+  },
+  target: 'node',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: moduleObj,
+  externals: [nodeExternals()]
+};
+module.exports = [client, server];
